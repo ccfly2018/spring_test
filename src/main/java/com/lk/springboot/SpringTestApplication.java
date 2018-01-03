6af245a11d7503d6592b5b0e3fd0d96613508e3f
陆昆 <lukun.cc@alibaba-inc.com>
2018-01-03 23:39:14 +0800

    first commit

---
+++ src/main/java/com/lk/springboot/SpringTestApplication.java
@@ -0,0 +1,12 @@
+package com.lk.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+}
